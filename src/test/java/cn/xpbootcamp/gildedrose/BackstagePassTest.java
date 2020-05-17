12f96acc4c4b4d0f98d34bea6c5c2f10da3a3b0f
Jiezhi.Xiao <jzxiao@thoughtworks.com>
2020-05-17 22:32:52 +0800

    impl: backstage pass, case 4

--- src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
@@ -47,4 +47,18 @@ public class BackstagePassTest {
         assertThat(newQuality, equalTo(oldQuality + 3));
         assertThat(newSellIn, equalTo(oldSellIn - 1));
     }
+
+    @Test
+    void should_quality_is_50_and_sellIn_reduce_1_when_a_day_pass_given_backstage_pass_quality_is_less_than_49_and_sellIn_is_greater_than_5_less_equal_than_10() {
+        double oldQuality = 48;
+        int oldSellIn = 1;
+
+        Goods goods = new Goods("BackstagePass", oldQuality, oldSellIn);
+        goods.updateByDay();
+        double newQuality = goods.getQuality();
+        int newSellIn = goods.getSellIn();
+
+        assertThat(newQuality, equalTo(50.0));
+        assertThat(newSellIn, equalTo(oldSellIn - 1));
+    }
 }
